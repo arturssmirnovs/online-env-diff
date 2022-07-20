c953c270f3846bc7fa1ff38580b6642d2b342696
Arturs Smirnovs <yam.aka.as@gmail.com>
2022-07-20 20:36:14 +0300

    Changed base path for github pages & build path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
+const path = require("path");
+
 module.exports = {
   runtimeCompiler: true,
-  publicPath: '/dist/'
+  publicPath: 'https://arturssmirnovs.github.io/online-env-diff/',
+  outputDir: path.resolve(__dirname, 'docs')
 }
